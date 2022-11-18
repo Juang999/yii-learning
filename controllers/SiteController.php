45b29dab58ef9818a7f234cf11932a67f8c8bef9
Bangkit Juang Raharjo <juangraharjo03@gmail.com>
2022-11-18 22:44:03 +0700

    update learning

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -125,4 +125,9 @@ class SiteController extends Controller
     {
         return $this->render('about');
     }
+
+    public function actionSay($message = 'hello world')
+    {
+        return $this->render('say', ['message' => $message]);
+    }
 }
